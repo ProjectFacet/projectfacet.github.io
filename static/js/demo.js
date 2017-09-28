9b27dc6b3148a22ee3968bb3f696aa7a2384031e
Heather Bryant <heather@rebelcompass.com>
2017-09-28 14:46:29 -0700

    updated to match new site

--- static/js/demo.js
+++ static/js/demo.js
@@ -7,8 +7,8 @@
 
 $(document).ready(function() {
   $('#particles').particleground({
-    dotColor: '#084250',
-    lineColor: '#0A2937'
+    dotColor: '#475266',
+    lineColor: '#394252'
   });
   $('.intro').css({
     'margin-top': -($('.intro').height() / 2)
